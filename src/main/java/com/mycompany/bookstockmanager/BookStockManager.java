88826e2ebfffdf1706917ecdb78db032cb3c3c30
IanZ <ian.zuniga@schoolmint.net>
2026-06-29 18:45:48 -0600

    Crear punto de entrada

--- src/main/java/com/mycompany/bookstockmanager/BookStockManager.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.bookstockmanager;
-
-public class BookStockManager {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/com/mycompany/bookstockmanager/Main.java
@@ -0,0 +1,9 @@
+package com.mycompany.bookstockmanager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Reportes_MenuPrincipal menu = new Reportes_MenuPrincipal();
+        menu.MostrarMenu();
+    }
+}
