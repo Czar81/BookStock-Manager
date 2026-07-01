3382eef1ec752f3fa0b2c8b4a782ea8cf3d70d1f
steven <stevenadriela1236@gmail.com>
2026-07-01 14:50:02 -0600

    inicie la creacion de la clase inventario, dejando listo todo para que en la clase Libro, se hagan los setters y getters

--- src/main/java/com/mycompany/bookstockmanager/Inventario.java
+++ src/main/java/com/mycompany/bookstockmanager/Inventario.java
@@ -1,7 +1,33 @@
 package com.mycompany.bookstockmanager;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class Inventario {
-    public void mostrarInventario() {
-        
+    private ArrayList<Libro> listaLibro;
+
+    public Inventario(){
+        this.listaLibro = new ArrayList<>();
+    }
+    
+    public boolean verificarStock(String titulo, int cantidad) {
+        for(Libro l : listaLibro){
+            if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                return l.getStock() >= cantidad;
+            }   
+
+        }
+    
+    
+        return true;
+    }
+
+    public void restarStock(String titulo, int cantidadARestar){
+        for(Libro l : listaLibro){
+            if (l.getTitulo().equalsIgnoreCase(titulo)){
+                int StockActual = l.getStock();
+                l.setStock(stockActual - cantidadARestar);
+            break;
+            }
+        }
     }
 }
